e40d3a4032f9943c46dad256142f30d8d136e338
Aakash <aakash4u.ab@gmail.com>
2020-04-04 03:37:56 +0530

    Initial Commit

---
+++ Swift Coding Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Coding Challenge
+//
+//  Created by Aakash Brahmbhatt on 04/04/20.
+//  Copyright © 2020 Aakash Brahmbhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
